d8d58939840c3ce02141b3cbecd8fd0ab51b7c4b
Dmitry Zaharov <98744363+Dimose75@users.noreply.github.com>
2023-06-14 18:41:36 +0400

    pages: create models

--- pages/models.py
+++ pages/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Post(models.Model):
+    """Модель содержит заголовок, слаг, тело поста, статус - публикация или черновик, дату публикации, создания и обновления.
+    Поле автора имеет связь с моделью User.
+    Имеет сортировку по убыванию: от новых к старым и индексацию по полю publish в БД."""
+
+    class Status(models.TextChoices):
+        DRAFT = 'DF', 'Draft'
+        PUBLISHED = 'PB', 'Published'
+    title = models.CharField(max_length=250)
+    slug = models.SlugField(max_length=250)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='pages_posts')
+    body = models.TextField()
+    publish = models.DateTimeField(default=timezone.now)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    status = models.CharField(max_length=2, choices=Status.choices, default=Status.DRAFT)
+
+    class Meta:
+        ordering = ['-publish']
+        indexes = [models.Index(fields=['-publish'])]
+
+    def __str__(self):
+        return self.title
