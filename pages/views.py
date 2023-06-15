a9dd819249f071d784cb8aec5eb1e3c0c71d8a8f
Dmitry Zaharov <98744363+Dimose75@users.noreply.github.com>
2023-06-15 16:36:35 +0400

    pages: upd post_detail

--- pages/views.py
+++ pages/views.py
@@ -1,21 +1,53 @@
 import os
 
 from django.core.mail import send_mail
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 from django.shortcuts import render, get_object_or_404
+from taggit.models import Tag
 from .models import Post, Comment
 from django.views.decorators.http import require_POST
 from django.views.generic import ListView
 from .forms import EmailPostForm, CommentForm
+from django.db.models import Count
 
 
-class PostListView(ListView):
-    queryset = Post.published.all()
-    context_object_name = 'posts'
-    paginate_by = 3
-    template_name = 'pages/post/list.html'
+def post_list(request, tag_slug=None):
+    post_list = Post.published.all()
+    tag = None
+    if tag_slug:
+        tag = get_object_or_404(Tag, slug=tag_slug)
+        post_list = post_list.filter(tags__in=[tag])
+    paginator = Paginator(post_list, 3)
+    page_number = request.GET.get('page', 1)
+    try:
+        posts = paginator.page(page_number)
+    except PageNotAnInteger:
+        posts = paginator.page(1)
+    except EmptyPage:
+        posts = paginator.page(paginator.num_pages)
+
+    return render(request, 'pages/post/list.html',
+                  {'posts': posts,
+                   'tag': tag})
+
+
+# class PostListView(ListView):
+#     queryset = Post.published.all()
+#     context_object_name = 'posts'
+#     paginate_by = 3
+#     template_name = 'pages/post/list.html'
 
 
 def post_detail(request, year, month, day, post):
+
+    """
+    Пояснение к агрегированному подсчету тегов через 'Count': 'post_tags_ids' - с помощью атрибута 'flat=True'
+    получаем список идентификаторов тегов текущего поста, через запятую([1,2,3]). 'similar_posts' - берем все посты,
+    включающие любой из тегов в 'post_tags_ids', за исключением текущего('exclude(id=post.id')). В 'same_tags'
+    считаем количество общих тегов, сортируем теги по числу общих тегов и дате публикации. Выводим только последние 4
+    поста.
+    """
+
     post = get_object_or_404(Post,
                              status=Post.Status.PUBLISHED,
                              slug=post,
@@ -25,10 +57,15 @@ def post_detail(request, year, month, day, post):
     comments = post.comments.filter(active=True)
     form = CommentForm()
 
+    post_tags_ids = Post.tags.values_list('id', flat=True)
+    similar_posts = Post.published.filter(tags__in=post_tags_ids).exclude(id=post.id)
+    similar_posts = similar_posts.annotate(same_tags=Count('tags')).order_by('-same_tags', '-publish')[:4]
+
     return render(request, 'pages/post/detail.html',
                   {'post': post,
                    'comments': comments,
-                   'form': form})
+                   'form': form,
+                   'similar_posts': similar_posts})
 
 
 def post_share(request, post_id):
