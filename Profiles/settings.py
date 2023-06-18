6caedf316d93c067b550744799861a0650fe7b80
Dmitry Zaharov <98744363+Dimose75@users.noreply.github.com>
2023-06-18 11:12:53 +0400

    pages: setup postgresql

--- Profiles/settings.py
+++ Profiles/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 import os
+import psycopg2
 from pathlib import Path
 from dotenv import load_dotenv
 
@@ -45,6 +46,7 @@ INSTALLED_APPS = [
     "taggit",
     "django.contrib.sites",
     "django.contrib.sitemaps",
+    "django.contrib.postgres",
 ]
 
 MIDDLEWARE = [
@@ -84,8 +86,12 @@ WSGI_APPLICATION = "Profiles.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.getenv('NAME'),
+        "USER": os.getenv('USER'),
+        "PASSWORD": os.getenv('PASSWORD'),
+        'HOST': 'localhost',
+        'PORT': '5433',
     }
 }
 
